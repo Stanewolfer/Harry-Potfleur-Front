25549bdf2a2995e6a3d700b32d1a35eb82201895
Stanewolfer <ethan.nghien1@gmail.com>
2025-04-01 16:51:41 +0200

    création sample page accueil

--- App.tsx
+++ App.tsx
@@ -1,12 +1,64 @@
-import React from 'react';
-import * as eva from '@eva-design/eva';
-import { ApplicationProvider, Layout, Button } from '@ui-kitten/components';
-import { default as theme } from './theme.json';
+import React from 'react'
+import * as eva from '@eva-design/eva'
+import {
+  ApplicationProvider,
+  Layout,
+  Text,
+  Divider,
+  Drawer,
+  DrawerItem
+} from '@ui-kitten/components'
+import { default as theme } from './theme.json'
 
 export default () => (
   <ApplicationProvider {...eva} theme={{ ...eva.dark, ...theme }}>
-    <Layout style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Button>Bienvenue sur Harry Potfleur</Button>
+    <Layout
+      style={{
+        flex: 1,
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        alignItems: 'flex-start'
+      }}
+    >
+      <Layout
+        style={{
+          justifyContent: 'flex-start',
+          alignItems: 'center',
+          padding: 10,
+          width: '100%'
+        }}
+      >
+        <Text category='h5' status='primary'>
+          Bienvenue sur Harry Potfleur
+        </Text>
+        <Text category='s1' status='primary'>
+          Une application de gestion de potfleurs
+        </Text>
+      </Layout> 
+      <Divider style={{ width: '100%' }} />
+      <Layout
+        style={{
+          justifyContent: 'flex-start',
+          alignItems: 'flex-start',
+          padding: 10,
+          width: '100%'
+        }}
+      >
+        <Text category='h5' status='success'>
+          Liste de vos plantes
+        </Text>
+        <Drawer
+          style={{
+            width: '100%',
+            height: '100%',
+          }}
+        >
+          <DrawerItem title='Roses' />
+          <DrawerItem title='Pivoines' />
+          <DrawerItem title='Tulipes' />
+          <DrawerItem title='Orchidées' />
+        </Drawer>
+      </Layout>
     </Layout>
   </ApplicationProvider>
-);
+)
